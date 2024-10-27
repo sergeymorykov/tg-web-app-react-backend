ed91fa234e35e025e2073ba9566ea5035ca4b0e9
Виктор Максимов <MirPngFut@yandex.ru>
2024-10-27 08:11:54 +0300

    added adel

---
+++ DB/clusters_umap.py
@@ -0,0 +1,119 @@
+
+import numpy as np
+import os
+
+project_path: str = 'C:/Users/ASUS/OneDrive/CODES/nodeJS/tg-web-app-react-backend/DB'
+
+# Visualize the 2D space of the new features
+def save_png(X_umap, Y_pred, id_user: int = 0):
+
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    ax.scatter(X_umap[:, 0], X_umap[:, 1], c=np.array(Y_pred))
+    ax.set_xlabel('umap Component 1')
+    ax.set_ylabel('umap Component 2')
+    user_x, user_y = X_umap[id_user, 0], X_umap[id_user, 1]
+    ax.scatter(user_x, user_y, c='black', s=15)
+    ax.annotate('you', (user_x, user_y), textcoords="offset points", xytext=(0,10), ha='center')
+    plt.savefig(f'{project_path}/umap.png')
+
+
+def get_label():
+    
+    data = read_data()
+    id_user = data.shape[0] - 1
+
+    from sklearn.neighbors import KNeighborsClassifier
+
+    X_umap = np.load(f'{project_path}/X_umap.npy')
+    Y_pred = np.load(f'{project_path}/Y_pred.npy')
+    knn = KNeighborsClassifier(n_neighbors=3).fit(X_umap, Y_pred)
+    user_cluster = knn.predict([data[id_user]])[0]
+
+    same_cluster_ids = [idx for idx, label in enumerate(Y_pred) if label == user_cluster and idx != id_user]
+    fullnames = [user['fullname'] for user in data if user['id_user'] in same_cluster_ids]
+    same_cluster_fullnames = ', '.join(fullnames)
+    
+    return user_cluster, same_cluster_fullnames
+
+
+def clusterize(data: np.ndarray, id_user: int = 0) -> np.ndarray:
+
+    from sklearn.cluster import KMeans
+
+    optimalNumOfClusters = 4
+    # maxNumOfClusters = int(data.shape[0] / 4)
+    # if maxNumOfClusters > 1:
+    #     startError = KMeans(n_clusters=1).fit(data).inertia_
+    #     for i in range(2, maxNumOfClusters):
+    #         if KMeans(n_clusters=i).fit(data).inertia_ < startError / 2:
+    #             optimalNumOfClusters = i
+    #             break
+    #     print(f"Optimal number of clusters: {optimalNumOfClusters}")
+    kmeans = KMeans(n_clusters=optimalNumOfClusters).fit(data)
+
+    # Получение значений классов после кластеризации
+    Y_pred = kmeans.labels_
+    user_cluster = Y_pred[id_user]
+    same_cluster_ids = [idx for idx, label in enumerate(Y_pred) if label == user_cluster and idx != id_user]
+    print(f"IDs in the same cluster as user {id_user}: {same_cluster_ids}")
+
+    return Y_pred
+
+
+def read_data() -> np.ndarray:
+    import json
+
+    # Load the JSON data from the file. 17 features
+    with open(f'{project_path}/users.json') as file:
+        database = json.load(file)
+
+    data = [user['interests_values'] for user in database]
+    if data is None:
+        print('error: no data')
+        exit()
+
+    return np.array(data)
+
+
+def read_sample_data() -> np.ndarray:
+
+    import pandas as pd
+
+    data = pd.read_json(f'{project_path}/data100.json').values.transpose()
+    return data
+
+
+def clusterization():
+    data = read_data()
+    print(data)
+    id_user = data.shape[0] - 1
+    Y_pred = clusterize(data, id_user)
+
+    from umap import UMAP
+
+    X_umap = UMAP().fit_transform(data)
+    np.save(f'{project_path}/X_umap', X_umap)
+    np.save(f'{project_path}/Y_pred', Y_pred)
+    save_png(X_umap, Y_pred, id_user)
+
+
+if __name__ == '__main__':
+    if os.path.exists(f'{project_path}/X_umap.npy') and os.path.exists(f'{project_path}/Y_pred.npy'):
+        X_umap = np.load(f'{project_path}/X_umap.npy')
+        Y_pred = np.load(f'{project_path}/Y_pred.npy')
+        save_png(X_umap, Y_pred)
+    else:
+        data = read_data()
+        # data = read_sample_data()
+        print(data)
+        id_user = data.shape[0] - 1
+        Y_pred = clusterize(data, id_user)
+        
+        from umap import UMAP
+
+        X_umap = UMAP().fit_transform(data)
+        np.save(f'{project_path}/X_umap', X_umap)
+        np.save(f'{project_path}/Y_pred', Y_pred)
+        save_png(X_umap, Y_pred, id_user)
--- start_api.py
+++ start_api.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify,request, make_response
 import json
 from datetime import datetime
+import telebot
+from DB.clusters_umap import get_label
+
+token = '7866617284:AAHDOfPQJdKmufOdRgFza6XA8ZWRHPeA_Yc'
 
 interests = [
     "Стартапы, поиск команды и нетворкинг", 
@@ -24,8 +28,14 @@ interests = [
 
 interests_values = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
 
+# Создаем экземпляр бота
+bot = telebot.TeleBot(token)
+
+
 app = Flask(__name__)
 
+
+last_id = ""
 
 @app.route('/get_event', methods=['GET', 'OPTIONS'])
 def get_event():
@@ -49,7 +59,6 @@ def get_event():
     response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'  # Разрешенные методы
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Разрешенные заголовки
     return response
-
 
 @app.route('/add_event', methods=['POST', 'OPTIONS'])
 def add_event():
@@ -89,14 +98,60 @@ def add_event():
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Разрешенные заголовки
     return response
 
+# API для отправки сообщения о регистрации
+@app.route('/last_chat_id', methods=['POST','GET','OPTIONS'])
+def last_id():
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response
+    data = request.json
+    print(f"Data: {data}")
+
+    last_id = data['last_id']
+    response = make_response(jsonify({"status":"ok"}))
+    response.headers['Access-Control-Allow-Origin'] = '*'  # Разрешить доступ с любых источников
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'  # Разрешенные методы
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Разрешенные заголовки
+    return response
+
+# API для отправки сообщения о регистрации
+@app.route('/register_event', methods=['POST','GET','OPTIONS'])
+def register_event():
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response
+    data = request.json
+
+
+    print(f"Data reg event: {data}")
+    user_id = data["user_id"]
+    event_name = data["event_name"]
+
+    if user_id and event_name:
+        # Отправляем сообщение в Telegram
+        bot.send_message(user_id, f"Вы зарегистрировались на событие: {event_name}")
+        response = make_response(jsonify({"status": "success", "message": "Notification sent"}))
+        response.headers['Access-Control-Allow-Origin'] = '*'  # Разрешить доступ с любых источников
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'  # Разрешенные методы
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Разрешенные заголовки
+
+        return response
+    else:
+        return jsonify({"status": "error", "message": "User ID or Event name is missing"}), 400
 
 @app.route('/users_reg', methods=['GET','POST','OPTIONS'])
 def users_reg():
     if request.method == 'OPTIONS':
         response = make_response()
         response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         return response
     print(request.method)
     json_data = request.get_json() 
@@ -114,7 +169,9 @@ def users_reg():
             id_new_user = int(user['id_user'])  * 7 + 1
 
     
-    new_user['id_tg'] = json_data['id']
+    if 'id' in json_data:
+        new_user['id_tg'] = json_data['id']
+        new_user['name'] = json_data['name']
     new_user['id_user'] = id_new_user
     new_user['fullname'] = json_data['fullname']
     new_user['photo'] = json_data['photo']
@@ -124,16 +181,21 @@ def users_reg():
         new_values[interests.index(interest)] = 1
     new_user['interests_values'] = new_values
 
-
     data.append(new_user)
     with open('./DB/users.json', 'w', encoding='utf-8') as f:
         json.dump(data, f)
-
+    
+    user_cluster, same_cluster_fullnames = get_label()
+    
+    if 'id' in json_data:
+        bot.send_message(json_data['id'], f"Вы зарегистрировались!")
+        bot.send_message(json_data['id'], f"Твой кластер: {user_cluster}, люди в твоем кластере: ({same_cluster_fullnames})")
+        
 
     response = make_response(jsonify({"status":"ok"}))
-    response.headers['Access-Control-Allow-Origin'] = '*'  # Разрешить доступ с любых источников
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'  # Разрешенные методы
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Разрешенные заголовки
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     return response
     
 
@@ -153,6 +215,53 @@ def get_users():
     response.headers['Access-Control-Allow-Origin'] = '*'  # Разрешить доступ с любых источников
     response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'  # Разрешенные методы
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Разрешенные заголовки
+    return response
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
+
+@app.route('/like', methods=['POST','OPTIONS','GET'])
+def likes():
+    if request.method == 'OPTIONS':
+        # Preflight request; respond with allowed methods and headers
+        response = make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        return response
+
+    json_data = request.get_json() 
+    print(f"JSON: {json_data}")
+
+
+    response = make_response(jsonify({"status":"ok"}))
+    response.headers['Access-Control-Allow-Origin'] = '*'  # Разрешить доступ с любых источников
+    response.headers['Access-Control-Allow-Methods'] = '*'  # Разрешенные методы
+    response.headers['Access-Control-Allow-Headers'] = '*'  # Разрешенные заголовки
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
+
+@app.route('/dislike', methods=['POST','OPTIONS','GET'])
+def dislikes():
+    if request.method == 'OPTIONS':
+        # Preflight request; respond with allowed methods and headers
+        response = make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        return response
+    
+    json_data = request.get_json() 
+    print(f"JSON: {json_data}")
+
+    response = make_response(jsonify({"status":"ok"}))
+    response.headers['Access-Control-Allow-Origin'] = '*'  # Разрешить доступ с любых источников
     return response
 
 
